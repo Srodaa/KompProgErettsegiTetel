21adbef5ce4f165b03f57f8629c8a087294fcc6c
szdavid <david030719@gmail.com>
2020-11-08 13:39:44 +0100

    Számolás cserélve lett.

--- script.js
+++ script.js
@@ -11,15 +11,3 @@ function download(){
     anchor.click();
     document.body.removeChild(anchor);
  }
-
-
-  
-function findTotal(){
-   var arr = document.getElementsByName('osszeg');
-   var tot=0;
-   for(var i=0;i<arr.length;i++){
-       if(parseInt(arr[i].value))
-           tot += parseInt(arr[i].value);
-   }
-   document.getElementById('total').value = tot;
-}
