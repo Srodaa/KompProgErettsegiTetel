3e9bbdbb0e599febf5dfdaa2be58ea77af263fa3
szdavid <david030719@gmail.com>
2020-11-11 17:07:16 +0100

    JS áthozva az ertekelolap.html-ből

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function Letoltes(){
     text = text.replace(/\n/g, "\r\n");
     var blob = new Blob([text], { type: "text/plain"});
     var anchor = document.createElement("a");
-    anchor.download = "my-filename.txt";
+    anchor.download = "Tételem.txt";
     anchor.href = window.URL.createObjectURL(blob);
     anchor.target ="_blank";
     anchor.style.display = "none";
@@ -11,3 +11,107 @@ function Letoltes(){
     anchor.click();
     document.body.removeChild(anchor);
  }
+
+ $('.table-group').on('input', '.prc', function () {
+   var totalSum = 0;
+   $('.table-group .prc').each(function () {
+     var inputVal = $(this).val();
+     if ($.isNumeric(inputVal)) {
+       totalSum += parseFloat(inputVal);
+     }
+   });
+   $('#result').val(totalSum);
+ });
+ $('.table-group').on('input', '.prc1', function () {
+   var totalSum = 0;
+   $('.table-group .prc1').each(function () {
+     var inputVal = $(this).val();
+     if ($.isNumeric(inputVal)) {
+       totalSum += parseFloat(inputVal);
+     }
+   });
+   $('#result1').val(totalSum);
+ });
+ $('.table-group').on('input', '.prc2', function () {
+   var totalSum = 0;
+   $('.table-group .prc2').each(function () {
+     var inputVal = $(this).val();
+     if ($.isNumeric(inputVal)) {
+       totalSum += parseFloat(inputVal);
+     }
+   });
+   $('#result2').val(totalSum);
+ });
+ $('.table-group').on('input', '.prc3', function () {
+   var totalSum = 0;
+   $('.table-group .prc3').each(function () {
+     var inputVal = $(this).val();
+     if ($.isNumeric(inputVal)) {
+       totalSum += parseFloat(inputVal);
+     }
+   });
+   $('#result3').val(totalSum);
+ });
+ $('.table-group').on('input', '.prc4', function () {
+   var totalSum = 0;
+   $('.table-group .prc4').each(function () {
+     var inputVal = $(this).val();
+     if ($.isNumeric(inputVal)) {
+       totalSum += parseFloat(inputVal);
+     }
+   });
+   $('#result4').val(totalSum);
+ });
+ $('.table-group').on('input', '.prc5', function () {
+   var totalSum = 0;
+   $('.table-group .prc5').each(function () {
+     var inputVal = $(this).val();
+     if ($.isNumeric(inputVal)) {
+       totalSum += parseFloat(inputVal);
+     }
+   });
+   $('#result5').val(totalSum);
+ });
+ $('.table-group').on('input', '.prc6', function () {
+   var totalSum = 0;
+   $('.table-group .prc6').each(function () {
+     var inputVal = $(this).val();
+     if ($.isNumeric(inputVal)) {
+       totalSum += parseFloat(inputVal);
+     }
+   });
+   $('#result6').val(totalSum);
+ });
+ $('.table-group').on('input', '.prc7', function () {
+   var totalSum = 0;
+   $('.table-group .prc7').each(function () {
+     var inputVal = $(this).val();
+     if ($.isNumeric(inputVal)) {
+       totalSum += parseFloat(inputVal);
+     }
+   });
+   $('#result7').val(totalSum);
+ });
+ $('.table-group').on('input', '.prc8', function () {
+   var totalSum = 0;
+   $('.table-group .prc8').each(function () {
+     var inputVal = $(this).val();
+     if ($.isNumeric(inputVal)) {
+       totalSum += parseFloat(inputVal);
+     }
+   });
+   $('#result8').val(totalSum);
+ });
+ $('.table-group').on('input', '.prc8', function () {
+   var totalSum = 0;
+   $('.table-group .prc8').each(function () {
+     var inputVal = $(this).val();
+     if ($.isNumeric(inputVal)) {
+       totalSum += parseFloat(inputVal);
+     }
+   });
+   $('#result8').val(totalSum);
+ });
+
+
+
